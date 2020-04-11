3c9dff42c8086671910bfc8f77d14353dbab32a0
Chris Penner <christopher.penner@gmail.com>
2020-04-11 14:28:29 -0600

    Run in subshell

--- copy-pasta.plugin.zsh
+++ copy-pasta.plugin.zsh
@@ -4,11 +4,14 @@
 
 local copy_pasta_folder="/tmp/copy-pasta"
 
-copy() {
+# Operates in a subshell so we can exit on errors for safety
+copy() {(
     # Exit on missing vars or failure for safety
     set -u
     set -e
     set -x
+    # Don't print errors on glob fail
+    setopt +o nomatch
 
     if [ $# -lt 1 ]; then
         cat << EOF
@@ -18,20 +21,24 @@ EOF
     fi
 
     if [ -d "$copy_pasta_folder" ]; then
-        rm -rf "${copy_pasta_folder:?}"/{*,.*}
+        rm -rf "${copy_pasta_folder:?}"/{*,.*} 2>/dev/null || true
     else
         mkdir "$copy_pasta_folder"
     fi
 
     cp -r "$@" "$copy_pasta_folder"
 
-}
+)}
 
-pasta() {
+# Operates in a subshell so we can exit on errors for safety
+pasta() {(
     # Exit on missing vars or failure for safety
     set -u
     set -e
     set -x
+    # Don't print errors on glob fail
+    setopt +o nomatch
+
     if [ $# -gt 1 ]; then
         cat >&2 << EOF
 Usage:
@@ -40,7 +47,7 @@ Usage:
 If destination dir is omitted the current directory is used.
 EOF
         return 1
-    elif [ $# == 0 ]; then
+    elif [ $# -eq 0 ]; then
         local dest="."
     else
         local dest="$1"
@@ -53,5 +60,5 @@ EOF
         return 1
     fi
 
-    cp -r "$copy_pasta_folder"/{*,.*} "$dest"
-}
+    cp -r "$copy_pasta_folder"/{*,.*} "$dest" 2>/dev/null || true
+)}
